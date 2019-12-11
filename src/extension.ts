a08693db0775a433dcb6b435153d0bd63e1017b5
Mitch Mommers <mitchoost@gmail.com>
2019-12-11 23:41:33 +0100

    Added better support for on save builds

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,57 @@
 import * as vscode from 'vscode';
-import { existsSync, lstatSync } from 'fs';
-import { join, sep } from 'path';
-import { spawnSync } from 'child_process';
+import {
+	existsSync,
+	lstatSync
+} from 'fs';
+import {
+	join,
+	sep,
+} from 'path';
+import {
+	spawn,
+} from 'child_process';
 
-function checkRecursive(path: string[]) {
+
+const flags = {
+	busy: false,
+	startTime: new Date().toISOString(),
+};
+
+enum BUILD_RESULT {
+	SUCCESS,
+	ERROR,
+	WARNING,
+}
+
+import {
+	groupBy,
+	omit,
+	mapValues
+} from "lodash";
+
+type diagEntryType = {
+	file: string,
+	diagnostic: vscode.Diagnostic
+};
+
+const configuration = vscode.workspace.getConfiguration('tsCompileOnSave');
+
+
+async function checkRecursive(path: string[], document: vscode.TextDocument): Promise < {
+	result: BUILD_RESULT,
+	diagnostics: diagEntryType[]
+} > {
+	let result: BUILD_RESULT = BUILD_RESULT.SUCCESS;
+
+	const diagnostics: diagEntryType[] = [];
+
 	while (path.length > 0) {
 
 		path.pop();
 		const cpath = path.join(sep);
-		if (!cpath) { break; }
+		if (!cpath) {
+			break;
+		}
 
 		const tsconfig = join(cpath, 'tsconfig.json');
 		if (!lstatSync(cpath).isDirectory()) {
@@ -19,45 +62,181 @@ function checkRecursive(path: string[]) {
 			continue;
 		}
 
-		vscode.window.showInformationMessage('Building folder: ' + cpath);
-			
-		spawnSync('tsc', {
-			cwd: cpath,
+		vscode.window.showInformationMessage('TSCompileOnSave: Started');
+
+		const output: string[] = await new Promise(resolve => {
+			const output: string[] = [];
+
+			const command = spawn('tsc', {
+				cwd: cpath,
+			});
+
+			command.stdout.on('data', function (data: Buffer) {
+				output.push(data.toString());
+			});
+			command.on('close', function () {
+				resolve(output);
+			});
+		});
+
+
+		output.forEach(v => {
+
+			const current = v.includes('warning') ?
+				BUILD_RESULT.WARNING : v.includes('error') ?
+				BUILD_RESULT.ERROR : BUILD_RESULT.SUCCESS;
+
+				console.log(v);
+
+			const diag = createDiagnostic(v, current);
+			if (diag) {
+				diagnostics.push(diag);
+			}
+
+			if (result !== BUILD_RESULT.ERROR && current === BUILD_RESULT.WARNING) {
+				result = BUILD_RESULT.WARNING;
+			} else if (current === BUILD_RESULT.ERROR) {
+				result = BUILD_RESULT.ERROR;
+			}
 		});
 
 		break;
 	}
-}	
 
-function build (fullFilePath: string) {
+	return {
+		result,
+		diagnostics
+	};
+}
+
+async function build(fullFilePath: string, document: vscode.TextDocument, collection: vscode.DiagnosticCollection) {
 	const path = fullFilePath.split(sep);
 
+	if (flags.busy) {
+		return vscode.window.showWarningMessage('Saved to quickly, the previous build is still running!');
+	}
+
 	try {
-		checkRecursive(path);
-		vscode.window.showInformationMessage('Finished building ts for path!');
+		flags.busy = true;
+		const {
+			result,
+			diagnostics
+		} = await checkRecursive(path, document);
+		switch (result) {
+			case BUILD_RESULT.SUCCESS:
+				vscode.window.showInformationMessage('Finished building ts!');
+				break;
+
+			case BUILD_RESULT.ERROR:
+				vscode.window.showErrorMessage('Failed to build ts because of errors!');
+				break;
+
+			case BUILD_RESULT.WARNING:
+				vscode.window.showWarningMessage('Finished building ts with warnings!');
+				break;
+		}
+		collection.clear();
+		updateDiagnostics(document, collection, diagnostics);
 	} catch (ex) {
+		console.log(ex);
 		vscode.window.showErrorMessage('Failed to build');
+	} finally {
+		flags.busy = false;
+	}
+}
+
+function processWarningDiagnostic(message: string, start: number, end: number, code: string): vscode.Diagnostic {
+	return {
+		code,
+		message: message,
+		range: new vscode.Range(new vscode.Position(start - 1, end), new vscode.Position(start - 1, end)),
+		severity: vscode.DiagnosticSeverity.Error,
+		source: 'TSCompileOnSave',
+		relatedInformation: []
+	};
+}
+
+function processErrorDiagnostic(message: string, start: number, end: number, code: string): vscode.Diagnostic {
+	return {
+		code,
+		message: message,
+		range: new vscode.Range(new vscode.Position(start - 1, end), new vscode.Position(start - 1, end)),
+		severity: vscode.DiagnosticSeverity.Warning,
+		source: 'TSCompileOnSave',
+		relatedInformation: []
+	};
+}
+
+function createDiagnostic(logMessage: string, tscError: BUILD_RESULT): diagEntryType | undefined {
+
+	const [fileInfo, errorCode, error] = logMessage.split(":");
+
+	const value = fileInfo.match(/([\w\/.]*)\((\d+),(\d+)\)/);
+	if (!value) {
+		return;
+	}
+
+	const file = value[1];
+
+	const nStart = parseInt(value[2]);
+	const nEnd = parseInt(value[3]);
+
+	let diagnostic: vscode.Diagnostic | undefined;
+	switch (tscError) {
+		case BUILD_RESULT.ERROR:
+			diagnostic = processErrorDiagnostic(error.trim(), nStart, nEnd, errorCode);
+		case BUILD_RESULT.WARNING:
+			diagnostic = processWarningDiagnostic(error.trim(), nStart, nEnd, errorCode);
+	}
+
+	if (!diagnostic) {
+		return;
+	}
+
+	return {
+		file,
+		diagnostic,
+	};
+}
+
+function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, diagnostics: diagEntryType[]): void {
+	if (document) {
+		const mapped = mapValues(groupBy(diagnostics, 'file'), clist => clist.map(v => omit(v, 'file')));
+		Object.keys(mapped).forEach(v => {
+			const d = mapped[v].map(v => v.diagnostic);
+			collection.set(vscode.Uri.file(v), d);
+		});
 	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "tscompileonsave" is now active!');
+	const collection = vscode.languages.createDiagnosticCollection("test");
 
 	let disposable = vscode.commands.registerCommand('extension.build', () => {
 		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) { return; }
+		if (!activeEditor) {
+			return;
+		}
 
-		console.log(activeEditor.document.fileName);
-
-		build(activeEditor.document.fileName);
+		build(activeEditor.document.fileName, activeEditor.document, collection);
 	});
 
 	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
-		// todo: implement this part with configuration
+		if (!configuration.get('enabled')) {
+			return;
+		}
+		if (!e.fileName.endsWith('.ts')) {
+			return;
+		}
+
+		build(e.fileName, e, collection);
 	});
 
+
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(collection);
 }
 
 export function deactivate() {}
