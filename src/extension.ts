86cc17d1e042680ff6fa0a44094cc0a0dc4670db
Mitch Mommers <mitchoost@gmail.com>
2020-01-25 16:41:02 +0100

    Added better handling of multiple processes and projects in the same window

--- src/extension.ts
+++ src/extension.ts
@@ -1,244 +1,301 @@
-import * as vscode from 'vscode';
-import {
-	existsSync,
-	lstatSync
-} from 'fs';
-import {
-	join,
-	sep,
-} from 'path';
-import {
-	spawn, ChildProcess,
-} from 'child_process';
-
-
-const flags = {
-	busy: false,
-	startTime: new Date().toISOString(),
-};
+import * as vscode from "vscode";
+import { existsSync, lstatSync } from "fs";
+import { join, sep } from "path";
+import { spawn, ChildProcess } from "child_process";
 
 enum BUILD_RESULT {
-	SUCCESS,
-	ERROR,
-	WARNING,
+    SUCCESS,
+    ERROR,
+    WARNING,
+    SKIP
 }
 
-import {
-	groupBy,
-	omit,
-	mapValues
-} from "lodash";
+import { groupBy, omit, mapValues } from "lodash";
 
 type diagEntryType = {
-	file: string,
-	diagnostic: vscode.Diagnostic
+    file: string;
+    diagnostic: vscode.Diagnostic;
 };
 
-const configuration = vscode.workspace.getConfiguration('tsCompileOnSave');
-let process: ChildProcess | null = null;
+const configuration = vscode.workspace.getConfiguration("tsCompileOnSave");
+let process: {
+	[k: string]: ChildProcess | null;
+} = {};
 
-
-async function checkRecursive(path: string[], document: vscode.TextDocument): Promise < {
-	result: BUILD_RESULT,
-	diagnostics: diagEntryType[]
-} > {
-	let result: BUILD_RESULT = BUILD_RESULT.SUCCESS;
+async function checkRecursive(
+    path: string[],
+    document: vscode.TextDocument
+): Promise<{
+    result: BUILD_RESULT;
+	diagnostics: diagEntryType[];
+	projectName?: string,
+}> {
+    let result: BUILD_RESULT = BUILD_RESULT.SUCCESS;
 
 	const diagnostics: diagEntryType[] = [];
+	
+	let found = false;
+	let cpath = "";
+    while (path.length > 0 && !found) {
+        path.pop();
+        cpath = path.join(sep);
+        if (!cpath) {
+            break;
+        }
 
-	while (path.length > 0) {
+        const tsconfig = join(cpath, "tsconfig.json");
+        if (!lstatSync(cpath).isDirectory()) {
+            continue;
+        }
 
-		path.pop();
-		const cpath = path.join(sep);
-		if (!cpath) {
-			break;
+        if (!existsSync(tsconfig)) {
+            continue;
 		}
 
-		const tsconfig = join(cpath, 'tsconfig.json');
-		if (!lstatSync(cpath).isDirectory()) {
-			continue;
-		}
+		found = true;
+	}
 
-		if (!existsSync(tsconfig)) {
-			continue;
-		}
+	if (!found) {
+		return {
+			result: BUILD_RESULT.SKIP,
+			diagnostics,
+		};
+	}
 
-		vscode.window.showInformationMessage('TSCompileOnSave: Started');
+	const projectName = path.pop() as string;
 
-		const output: string[] = await new Promise(resolve => {
-			const output: string[] = [];
+	if (process[projectName]) {
+		(process[projectName] as any).kill("SIGKILL");
+		process[projectName] = null;
+	}
+	
+	const output: string[] = await new Promise((resolve, reject) => {
+		const output: string[] = [];
 
-			const command = spawn('tsc', {
-				cwd: cpath,
-			});
-
-			process = command;
-
-			command.stdout.on('data', function (data: Buffer) {
-				output.push(data.toString());
-			});
-			command.on('close', function () {
-				resolve(output);
-			});
+		const command = spawn("tsc", {
+			cwd: cpath
 		});
 
+		process[projectName] = command;
 
+		command.stdout.on("data", function(data: Buffer) {
+			output.push(data.toString());
+		});
+		command.on("close", function(code, signal) {
+			console.log("signal, ", signal);
+			if (signal === "SIGKILL") {
+				return resolve();
+			}
+
+			process[projectName] = null;
+			return resolve(output);
+		});
+	});
+
+	if (!output) {
+		result = BUILD_RESULT.SKIP;
+	} else {
 		output.forEach(v => {
+			const current = v.includes("warning")
+				? BUILD_RESULT.WARNING
+				: v.includes("error")
+				? BUILD_RESULT.ERROR
+				: BUILD_RESULT.SUCCESS;
 
-			const current = v.includes('warning') ?
-				BUILD_RESULT.WARNING : v.includes('error') ?
-				BUILD_RESULT.ERROR : BUILD_RESULT.SUCCESS;
-
-				console.log(v);
+			console.log(v);
 
 			const diag = createDiagnostic(v, current);
 			if (diag) {
 				diagnostics.push(diag);
 			}
 
-			if (result !== BUILD_RESULT.ERROR && current === BUILD_RESULT.WARNING) {
+			if (
+				result !== BUILD_RESULT.ERROR &&
+				current === BUILD_RESULT.WARNING
+			) {
 				result = BUILD_RESULT.WARNING;
 			} else if (current === BUILD_RESULT.ERROR) {
 				result = BUILD_RESULT.ERROR;
 			}
 		});
-
-		break;
 	}
 
-	return {
-		result,
-		diagnostics
-	};
+    return {
+		projectName,
+        result,
+        diagnostics
+    };
 }
 
-async function build(fullFilePath: string, document: vscode.TextDocument, collection: vscode.DiagnosticCollection) {
-	const path = fullFilePath.split(sep);
+async function build(
+    fullFilePath: string,
+    document: vscode.TextDocument,
+    collection: vscode.DiagnosticCollection
+) {
+    const path = fullFilePath.split(sep);
 
-	if (process) {
-		process.kill("SIGKILL");
-	}
-
-	try {
-		const {
-			result,
-			diagnostics
-		} = await checkRecursive(path, document);
-		switch (result) {
+    try {
+        const { result, diagnostics, projectName } = await checkRecursive(path, document);
+        switch (result) {
 			case BUILD_RESULT.SUCCESS:
-				vscode.window.showInformationMessage('Finished building ts!');
+				vscode.window.showInformationMessage(
+					projectName + ": Finished without issues"
+				);
 				break;
+            case BUILD_RESULT.ERROR:
+                vscode.window.showErrorMessage(
+                    projectName + ": Failed to build ts because of errors!"
+                );
+                break;
 
-			case BUILD_RESULT.ERROR:
-				vscode.window.showErrorMessage('Failed to build ts because of errors!');
+            case BUILD_RESULT.WARNING:
+                vscode.window.showWarningMessage(
+                    projectName + ": Finished building ts with warnings!"
+                );
 				break;
-
-			case BUILD_RESULT.WARNING:
-				vscode.window.showWarningMessage('Finished building ts with warnings!');
+			case BUILD_RESULT.SKIP: 
+				console.log('Skipped');
 				break;
 		}
-		collection.clear();
-		updateDiagnostics(document, collection, diagnostics);
-	} catch (ex) {
-		console.log(ex);
-		vscode.window.showErrorMessage('Failed to build');
-	} finally {
-		process = null;
-	}
+		if (result !== BUILD_RESULT.SKIP) {
+			collection.clear();
+			updateDiagnostics(document, collection, diagnostics);
+		}
+    } catch (ex) {
+        console.log(ex);
+        vscode.window.showErrorMessage("Failed to build");
+    }
 }
 
-function processWarningDiagnostic(message: string, start: number, end: number, code: string): vscode.Diagnostic {
-	return {
-		code,
-		message: message,
-		range: new vscode.Range(new vscode.Position(start - 1, end), new vscode.Position(start - 1, end)),
-		severity: vscode.DiagnosticSeverity.Error,
-		source: 'TSCompileOnSave',
-		relatedInformation: []
-	};
+function processWarningDiagnostic(
+    message: string,
+    start: number,
+    end: number,
+    code: string
+): vscode.Diagnostic {
+    return {
+        code,
+        message: message,
+        range: new vscode.Range(
+            new vscode.Position(start - 1, end),
+            new vscode.Position(start - 1, end)
+        ),
+        severity: vscode.DiagnosticSeverity.Error,
+        source: "TSCompileOnSave",
+        relatedInformation: []
+    };
 }
 
-function processErrorDiagnostic(message: string, start: number, end: number, code: string): vscode.Diagnostic {
-	return {
-		code,
-		message: message,
-		range: new vscode.Range(new vscode.Position(start - 1, end), new vscode.Position(start - 1, end)),
-		severity: vscode.DiagnosticSeverity.Warning,
-		source: 'TSCompileOnSave',
-		relatedInformation: []
-	};
+function processErrorDiagnostic(
+    message: string,
+    start: number,
+    end: number,
+    code: string
+): vscode.Diagnostic {
+    return {
+        code,
+        message: message,
+        range: new vscode.Range(
+            new vscode.Position(start - 1, end),
+            new vscode.Position(start - 1, end)
+        ),
+        severity: vscode.DiagnosticSeverity.Warning,
+        source: "TSCompileOnSave",
+        relatedInformation: []
+    };
 }
 
-function createDiagnostic(logMessage: string, tscError: BUILD_RESULT): diagEntryType | undefined {
+function createDiagnostic(
+    logMessage: string,
+    tscError: BUILD_RESULT
+): diagEntryType | undefined {
+    const [fileInfo, errorCode, error] = logMessage.split(":");
 
-	const [fileInfo, errorCode, error] = logMessage.split(":");
+    const value = fileInfo.match(/([\w\/.]*)\((\d+),(\d+)\)/);
+    if (!value) {
+        return;
+    }
 
-	const value = fileInfo.match(/([\w\/.]*)\((\d+),(\d+)\)/);
-	if (!value) {
-		return;
-	}
+    const file = value[1];
 
-	const file = value[1];
+    const nStart = parseInt(value[2]);
+    const nEnd = parseInt(value[3]);
 
-	const nStart = parseInt(value[2]);
-	const nEnd = parseInt(value[3]); 
+    let diagnostic: vscode.Diagnostic | undefined;
+    switch (tscError) {
+        case BUILD_RESULT.ERROR:
+            diagnostic = processErrorDiagnostic(
+                error.trim(),
+                nStart,
+                nEnd,
+                errorCode
+            );
+        case BUILD_RESULT.WARNING:
+            diagnostic = processWarningDiagnostic(
+                error.trim(),
+                nStart,
+                nEnd,
+                errorCode
+            );
+    }
 
-	let diagnostic: vscode.Diagnostic | undefined;
-	switch (tscError) {
-		case BUILD_RESULT.ERROR:
-			diagnostic = processErrorDiagnostic(error.trim(), nStart, nEnd, errorCode);
-		case BUILD_RESULT.WARNING:
-			diagnostic = processWarningDiagnostic(error.trim(), nStart, nEnd, errorCode);
-	}
+    if (!diagnostic) {
+        return;
+    }
 
-	if (!diagnostic) {
-		return;
-	}
-
-	return {
-		file,
-		diagnostic,
-	};
+    return {
+        file,
+        diagnostic
+    };
 }
 
-function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, diagnostics: diagEntryType[]): void {
-	if (document) {
-		const mapped = mapValues(groupBy(diagnostics, 'file'), clist => clist.map(v => omit(v, 'file')));
-		Object.keys(mapped).forEach(v => {
-			const d = mapped[v].map(v => v.diagnostic);
-			collection.set(vscode.Uri.file(v), d);
-		});
-	}
+function updateDiagnostics(
+    document: vscode.TextDocument,
+    collection: vscode.DiagnosticCollection,
+    diagnostics: diagEntryType[]
+): void {
+    if (document) {
+        const mapped = mapValues(groupBy(diagnostics, "file"), clist =>
+            clist.map(v => omit(v, "file"))
+        );
+        Object.keys(mapped).forEach(v => {
+            const d = mapped[v].map(v => v.diagnostic);
+            collection.set(vscode.Uri.file(v), d);
+        });
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
+    const collection = vscode.languages.createDiagnosticCollection("test");
 
-	const collection = vscode.languages.createDiagnosticCollection("test");
+    let disposable = vscode.commands.registerCommand("extension.build", () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            return;
+        }
 
-	let disposable = vscode.commands.registerCommand('extension.build', () => {
-		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-			return;
+        build(
+            activeEditor.document.fileName,
+            activeEditor.document,
+            collection
+        );
+    });
+
+    vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
+        if (!configuration.get("enabled")) {
+            return;
+        }
+        if (!e.fileName.endsWith(".ts")) {
+            return;
 		}
+		
+		console.log(process);
 
-		build(activeEditor.document.fileName, activeEditor.document, collection);
-	});
+        build(e.fileName, e, collection);
+    });
 
-	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
-		if (!configuration.get('enabled')) {
-			return;
-		}
-		if (!e.fileName.endsWith('.ts')) {
-			return;
-		}
-
-		build(e.fileName, e, collection);
-	});
-
-
-
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(collection);
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(collection);
 }
 
 export function deactivate() {}
